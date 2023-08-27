38e226f49b1a0c09fde6d921272db8b56509aac5
prikolist <vladik008481@gmail.com>
2023-08-27 19:54:13 +0500

    1

--- main.py
+++ main.py
@@ -59,11 +59,35 @@ async def VideoPage(request, filename):
 
 @app.route('/servevideo/<filename:str>')
 async def serve_video(request, filename):
-    return await response.file_stream('video/'+filename)
+    video_path = 'video/' + filename
+
+    # Открываем файл видео
+    with open(video_path, 'rb') as video_file:
+        video_data = video_file.read()
+
+    headers = {'Accept-Ranges': 'bytes'}
+    content_range = request.headers.get('Range')
+
+    if content_range:
+        # Разбираем значение Range заголовка
+        start, end = content_range.replace('bytes=', '').split('-')
+        start = int(start)
+        end = int(end) if end else len(video_data) - 1
+
+        # Определяем длину контента и формируем заголовок Content-Range
+        content_length = end - start + 1
+        headers['Content-Range'] = f'bytes {start}-{end}/{len(video_data)}'
+        
+        # Вырезаем запрошенный диапазон данных из файла
+        video_chunk = video_data[start:end+1]
+        return response.raw(video_chunk, headers=headers, status=206)
+
+    # Если Range не указан, отправляем весь файл
+    return await response.file(video_path, headers=headers)
 
 @app.route('/image/<filename:str>')
 async def serve_image(request, filename):
-    return await response.file_stream('Images/'+filename)
+    return await response.file('Images/'+filename)
      
 
 @app.route('/')
